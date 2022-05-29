404e5f49e53169de399e1bb05e577ba9c908139b
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:18:25 +0300

    Update task_1_cezar.py

--- topic_4/task_1_cezar.py
+++ topic_4/task_1_cezar.py
@@ -16,3 +16,10 @@
 Формат выходных данных:
 Вывести одну строку – декодированное сообщение.
 """
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+n = int(input())
+s = input().strip()
+res = ''
+for c in s:
+    res += alpha[(alpha.index(c) - n) % len(alpha)]
+print(res)
