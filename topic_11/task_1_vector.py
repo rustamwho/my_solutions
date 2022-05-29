3d1dfd86ddc6ba46bda6e586fc98160be7563b38
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:33:19 +0300

    Update task_1_vector.py

--- topic_11/task_1_vector.py
+++ topic_11/task_1_vector.py
@@ -7,3 +7,25 @@
 Перегрузить операторы:
     __add__, __sub__, __mul__,__floordiv__,__truediv__, __mod__.
 """
+class Position(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Position(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Position(self.x * other.x, self.y * other.y)
+
+    def __floordiv__(self, other):
+        return Position(self.x // other.x, self.y // other.y)
+
+    def __truediv__(self, other):
+        return Position(self.x / other.x, self.y / other.y)
+
+    def __mod__(self, other):
+        return Position(self.x % other.x, self.y % other.y)
