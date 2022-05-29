99568dfa100537b1e76363cca6cee140a9808dd2
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:26:24 +0300

    Update task_4_shared_numbers.py

--- topic_7/task_4_shared_numbers.py
+++ topic_7/task_4_shared_numbers.py
@@ -14,3 +14,10 @@
 Вывести цифры в соответствии с условием задачи. Если общих цифр нет, то ничего
 выводить не нужно.
 """
+n = int(input())
+digits = [set(input()) for _ in range(n)]
+myset = set(digits[0])
+for i in range(1, n):
+    myset.intersection_update(digits[i])
+
+print(*sorted(myset))
