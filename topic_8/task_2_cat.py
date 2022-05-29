5995caf35bf4c78af0a5a329478ded97156520fd
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:28:12 +0300

    Update task_2_cat.py

--- topic_8/task_2_cat.py
+++ topic_8/task_2_cat.py
@@ -11,3 +11,10 @@ Cat.
 Формат выходных данных:
 Последовательность строк.
 """
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(r'\bcat\b', line):
+        print(line)
