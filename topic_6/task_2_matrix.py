a8b986c60470c1105d18983523ec607d2830cafd
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:21:31 +0300

    Update task_2_matrix.py

--- topic_6/task_2_matrix.py
+++ topic_6/task_2_matrix.py
@@ -17,3 +17,15 @@
 5  6  7  8
 9  10 11 12
 """
+n, m = map(int, input().split())
+matrix = []
+counter = 1
+
+for i in range(n):
+    matrix.append([])
+    for j in range(m):
+        matrix[i].append(counter)
+        counter += 1
+
+        print(f'{matrix[i][j]}'.ljust(3), end='')
+    print()
