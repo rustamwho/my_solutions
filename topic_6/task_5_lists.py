53b7e9d28080acf12fcbfc57633969bab701de46
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:23:56 +0300

    Update task_5_lists.py

--- topic_6/task_5_lists.py
+++ topic_6/task_5_lists.py
@@ -11,3 +11,10 @@
     6. Вывел список, с помощью функции print()
 """
 numbers = [8, 9, 10, 11]
+
+numbers[1] = 17
+numbers.extend([4, 5, 6])
+del numbers[0]
+numbers *= 2
+numbers.insert(3, 25)
+print(numbers)
