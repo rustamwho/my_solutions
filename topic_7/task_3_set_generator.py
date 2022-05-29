45bd099ff90820a7e28d0851e4eaa6a0e0ea4813
Zakirov Rustam <40338874+rustamwho@users.noreply.github.com>
2022-05-29 22:25:55 +0300

    Update task_3_set_generator.py

--- topic_7/task_3_set_generator.py
+++ topic_7/task_3_set_generator.py
@@ -14,3 +14,8 @@
 """
 items = [10, '30', 30, 10, '56', 34, '12', 90, 89, 34, 45, '67', 12, 10, 90,
          23, '45', 56, '56', 1, 5, '6', 5]
+
+myset = {int(c) for c in items}
+myset = sorted(myset)
+myset.pop()
+print(*myset)
